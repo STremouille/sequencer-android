4f40fe0b8a8eabb17c639284bfe4572f0fab7d00
samuel.tremouille@gmail.com <samuel.tremouille@gmail.com@7c214f79-d4ef-e4e8-166e-c58234a1747d>
2012-12-20 15:02:04 +0000

    Fix 4 line instrument

--- trunk/Sequencer/src/fr/uha/ensisa/sequencer/MusicTask.java
+++ trunk/Sequencer/src/fr/uha/ensisa/sequencer/MusicTask.java
@@ -12,29 +12,31 @@ public class MusicTask extends TimerTask {
 	ArrayList<MediaPlayer> mp;
 	HashMap<Integer, ArrayList<CheckBox>> cb;
 	int waitBetweenStep;
-	public MusicTask(ArrayList<MediaPlayer> mp,HashMap<Integer, ArrayList<CheckBox>> cb,float tempo)
-	{
-		this.mp=mp;
-		this.cb=cb;
-		this.waitBetweenStep=(int) ((60.0/tempo)*1000.0);
-		Log.i("tempo", "Step : "+60.0/(tempo));
+
+	public MusicTask(ArrayList<MediaPlayer> mp,
+			HashMap<Integer, ArrayList<CheckBox>> cb, float tempo) {
+		this.mp = mp;
+		this.cb = cb;
+		this.waitBetweenStep = (int) ((60.0 / tempo) * 1000.0);
+		Log.i("tempo", "Step : " + 60.0 / (tempo));
 	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		for (int mpNumber= 0 ; mpNumber<mp.size();mpNumber++)
-		{
-			for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 4; i++) {
+			for (int mpNumber = 0; mpNumber < mp.size(); mpNumber++) {
 				if (cb.get(mpNumber).get(i).isChecked()) {
 					mp.get(mpNumber).seekTo(0);
 					mp.get(mpNumber).start();
 				}
-				try {
-					Thread.currentThread().sleep(waitBetweenStep);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			}
+
+			try {
+				Thread.currentThread().sleep(waitBetweenStep);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+
 			}
 		}
 	}
